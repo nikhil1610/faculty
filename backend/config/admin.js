5c401ae1e49e0ab5f1edbe9d60e0eb60f0ed6a05
nikhil1610 <nikhilgautam106@gmail.com>
2022-04-21 15:27:29 +0530

    update

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -3,6 +3,6 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','4a0616a913cbc2ffcaecc9df9fd5343e3fa04c3aa96ea7979a2334183a47dab6'),
   },
 });
