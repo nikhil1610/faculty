975790ab1c960dfbf866552da1fc426796ab66a9
nikhil1610 <nikhilgautam106@gmail.com>
2022-04-21 13:20:49 +0530

    added app keys

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ["DATABASE_URL", "MY_HEROKU_URL", "NODE_ENV"]),
   },
 });
